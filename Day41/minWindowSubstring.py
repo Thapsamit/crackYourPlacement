845c47311c28f339c9a6aa6c89c1165dd9786d1f
Amit thapliyal <thapliyalamit2001@gmail.com>
2022-07-15 21:48:13 +0530

    day41

---
+++ Day41/minWindowSubstring.py
@@ -0,0 +1,51 @@
+from collections import defaultdict
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        dm = defaultdict(lambda:0)
+        hm = defaultdict(lambda:0)
+        for i in range(0,len(t),1):
+            dm[t[i]]+=1
+            
+        count = 0
+        dmCount = sum(dm.values())
+        start = -1
+        end = -1
+        ans=""
+        mini = 999999
+    
+        while True:
+            f1 = False
+            f2 = False
+            while end<len(s)-1 and count<dmCount:
+                end+=1
+                ch = s[end]
+                hm[ch]+=1
+                if ch in dm:
+                    if hm[ch]<=dm[ch]:
+
+                        count+=1
+                f1 = True
+        
+            while start<end and count==dmCount:
+                if end-(start+1)-1<mini:
+                    ans = s[start+1:end+1]
+                    mini = min(mini,end-(start+1)-1)
+                    
+                start+=1
+                ch = s[start]
+                if hm[ch]==1:
+                    hm.pop(ch)
+                else:
+                    hm[ch]-=1
+                if ch in dm:
+                    if hm[ch]<dm[ch]:
+                        count-=1
+                f2 = True
+            if f1==False and f2==False:
+                break
+        return ans
+                
+        
+                
+                
+        
